f1bc33a89c5ffd13facc88403c4cf7688fa91072
imataliev071 <imataliev071@gmail.com>
2023-12-25 10:45:02 +0600

    hw-4

--- tv_shows/urls.py
+++ tv_shows/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('film_list', views.tv_shows_list, name='film_list'),
-    path('film_list/<int:id>/', views.tv_shows_detail , name='tv_shows_detail'),
+    path('', views.tv_shows_list, name='film_list'),
+    path('film_list/<int:id>/', views.tv_shows_detail, name='tv_shows_detail'),
+    path('tv_shows_create/', views.tv_shows_create, name='tv_shows_create'),
+    path('tv_shows_delete/', views.tv_shows_delete, name="tv_shows_list_delite"),
+    path('tv_shows_drop/<int:id>/delete/', views.tv_drop_view, name='tv_shows_drop'),
+    path('tv_shows_list_update/', views.tv_shows_edit_view, name="tv_shows_list_update"),
+    path('tv_shows_update/<int:id>/update/', views.tv_shows_update, name="tv_shows_update"),
+
 ]
--- tv_shows/views.py
+++ tv_shows/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from . import models
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
+from . import models, forms
+from django.urls import reverse
 
 
 # не польная информация
@@ -13,3 +15,50 @@ def tv_shows_detail(request, id):
     if request.method == 'GET':
         tv_shows_id = get_object_or_404(models.TvShow, id=id)
         return render(request, 'tv/tv_shows_detail.html', {'tv_shows_id': tv_shows_id})
+
+
+def tv_shows_create(request):
+    if request.method == 'POST':
+        form = forms.TvShowForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('film_list'))
+    else:
+        form = forms.TvShowForm()
+        return render(request, 'crud/tv_shows_create.html', {'form': form})
+
+
+def tv_shows_delete(request):
+    if request.method == 'GET':
+        tv_shows_delite = models.TvShow.objects.all()
+        return render(request, 'crud/delete/tv_shows_list_delite.html', {'tv_shows_delite': tv_shows_delite})
+
+
+def tv_drop_view(request, id):
+    tv_shows_id = get_object_or_404(models.TvShow, id=id)
+    tv_shows_id.delete()
+    # return HttpResponse('удален')
+    return redirect(reverse('tv_shows_list_delite'))
+
+
+def tv_shows_edit_view(request):
+    if request.method == 'GET':
+        tv_shows_update = models.TvShow.objects.all()
+        return render(request, 'crud/update/tv_shows_list_update.html',
+                      {'tv_shows_update': tv_shows_update})
+
+
+def tv_shows_update(request, id):
+    tv_shows_id = get_object_or_404(models.TvShow, id=id)
+    if request.method == 'POST':
+        form = forms.TvShowForm(instance=tv_shows_id, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('tv_shows_list_update'))
+    else:
+        form = forms.TvShowForm(instance=tv_shows_id)
+        return render(request, 'crud/update/tv_shows_update.html',
+                      {
+                          'form': form,
+                          'tv_shows_id': tv_shows_id
+                      })
